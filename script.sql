a20d63dcdb7c925aac436876415cad3444cd9d7c
MariePorquier <marieporquier@free.fr>
2019-12-06 16:49:38 +0100

    sqlpartie4

---
+++ script.sql
@@ -0,0 +1,15 @@
+--partie4
+--Exo1--
+--Insérez les données suivantes dans la table languages de la base webDevelopment :
+--• JavaScript, version 5 • PHP, version 5.2 • PHP, version 5.4 • HTML, version 5.1 • JavaScript, version 6 • JavaScript, version 7 • JavaScript, version 8 • PHP, version 7
+USE `webdeveloppment`;
+INSERT INTO `languagues`(versions, lechangementcestmaintenant)
+VALUES
+('Javascript','5'),
+('PHP','5.2'),
+('PHP','5.4'),
+('HTML','5.1'),
+('Javascript','6'),
+('Javascript','7'),
+('Javascript','8'),
+('PHP','7');
